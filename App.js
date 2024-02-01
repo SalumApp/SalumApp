2baa555d874a103bb29de328696d0c65a3cdf30f
Kugelblitz <21170940+kugel-blitz@users.noreply.github.com>
2024-01-31 22:17:15 -0500

    test: scaffolding; mock up home page to test build pipelines

---
+++ App.js
@@ -0,0 +1,56 @@
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import NewHome from "./src/pages/NewHome";
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const screenSize = {
+    width: useWindowDimensions().width,
+    height: useWindowDimensions().height,
+  };
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={NewHome}
+            initialParams={{ screenSize }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
