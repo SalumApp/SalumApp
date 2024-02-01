41ca43cba42cab6ce0651fce7c299f3b84fe8d0a
qing0git <qing0@purdue.edu>
2024-01-31 23:30:32 -0500

    test: trigger CI

--- src/assets/icons/NavIcon/NavIcon.js
+++
@@ -1,9 +0,0 @@
-export const NavIcon = {
-  add: require("./add.svg"),
-  add_bg: require("./add_bg.svg"),
-  budget: require("./budget.svg"),
-  home: require("./home.svg"),
-  transfer: require("./transfer.svg"),
-  user: require("./user.svg"),
-  sub: require("./sub.svg"),
-};
