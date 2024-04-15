a4b5a3ad9262f0d7ebf41bc832b6aef24dd05bd1
Kugel-Blitz <21170940+kugel-blitz@users.noreply.github.com>
2024-04-15 02:22:31 -0400

    refactor: integrating budget screen

--- src/components/Navigation/TopNav.tsx
+++ src/components/Navigation/TopNav.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 import ArrowRet from "../../assets/GlyphProvider/glyph/Arrow/Glyph/arrow-left.svg";
 import More from "../../assets/GlyphProvider/glyph/User Interface/Glyph/more.svg";
 
 interface TopNavProps {
-  title: string;
+  title?: string;
   titleColor?: string;
   bgColor?: string;
-  onLeftPress: () => void;
-  onRightPress: () => void;
+  onLeftPress?: () => void;
+  onRightPress?: () => void;
+  left?: ReactNode;
+  center?: ReactNode;
+  right?: ReactNode;
 }
 
 export const TopNav = ({
@@ -18,24 +21,39 @@ export const TopNav = ({
   titleColor = "#000000",
   onLeftPress,
   onRightPress,
+  left,
+  center,
+  right,
 }: TopNavProps) => {
+  const defaultLeft = onLeftPress && (
+    <TouchableOpacity onPress={onLeftPress} className="pl-5">
+      <ArrowRet fill={titleColor} width={32} height={32} />
+    </TouchableOpacity>
+  );
+
+  const defaultCenter = title && (
+    <Text
+      className="text-3xl font-bold flex-1 text-center"
+      style={{ color: titleColor }}
+    >
+      {title}
+    </Text>
+  );
+
+  const defaultRight = onRightPress && (
+    <TouchableOpacity onPress={onRightPress} className="pr-5">
+      <More fill={titleColor} width={32} height={32} />
+    </TouchableOpacity>
+  );
+
   return (
     <View
       style={{ backgroundColor: bgColor }}
       className="w-full bg-transparent flex-row items-center justify-between pt-4"
     >
-      <TouchableOpacity onPress={onLeftPress} className="pl-4">
-        <ArrowRet fill={titleColor} width={32} height={32} />
-      </TouchableOpacity>
-      <Text
-        className="text-3xl font-bold flex-1 text-center"
-        style={{ color: titleColor }}
-      >
-        {title}
-      </Text>
-      <TouchableOpacity onPress={onRightPress} className="pr-4">
-        <More fill={titleColor} width={32} height={32} />
-      </TouchableOpacity>
+      {left || defaultLeft}
+      {center || defaultCenter}
+      {right || defaultRight}
     </View>
   );
 };
--- src/pages/BudgetScreen.tsx
+++ src/pages/BudgetScreen.tsx
@@ -1,22 +1,61 @@
 import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { useQuery } from "@realm/react";
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 import { MyButton } from "../components/Button/Button";
-import { BudgetList } from "../components/List/BudgetList";
+import BudgetCard from "../components/Card/BudgetCard";
+import { Category } from "../models/Category";
+import { Transaction } from "../models/Transaction";
 import { SafeAreaInsetsView } from "../utils/SafeArea";
 
 export const BudgetScreen = () => {
   const navigation = useNavigation<NavigationProp<any>>();
+  // const categories = useQuery(Category);
+  const categories = useQuery(Category, (allCategory) => {
+    return allCategory.filtered(
+      "isExpense == $0 && hasBudget == $1",
+      true,
+      true,
+    );
+  });
 
   return (
-    <SafeAreaInsetsView className="flex-1 bg-s_blue-100">
-      <View className="h-4/5 bg-s_light-60 absolute w-full bottom-0 rounded-t-3xl">
+    <SafeAreaInsetsView className="flex-1 bg-s_blue-100 dark:bg-s_dark_navy-75">
+      <View className="h-4/5 bg-s_light-60 dark:bg-s_dark-100 absolute w-full bottom-0 rounded-t-3xl">
         <ScrollView>
           <View className="pt-2" />
-          <BudgetList />
+          {categories.length === 0 && (
+            <View className="flex-1 items-center justify-center h-full">
+              <Text className="flex-col text-2xl self-center align-middle">
+                No Data
+              </Text>
+            </View>
+          )}
+          {categories.length !== 0 && (
+            <View>
+              {categories.map((category) => {
+                let usedAmount = 0;
+                category
+                  .linkingObjects<Transaction>("Transaction", "category")
+                  .forEach((trans) => {
+                    usedAmount += trans.amount;
+                  });
+                return (
+                  <BudgetCard
+                    key={category.title}
+                    totalAmount={category.budget / 100}
+                    usedAmount={usedAmount / 100}
+                    categoryTitle={category.title}
+                    categoryColor={category.color}
+                    currency="USD"
+                  />
+                );
+              })}
+            </View>
+          )}
         </ScrollView>
-        <View className="h-20 mb-36">
+        <View className="h-20 mb-32">
           <MyButton
             onPress={() => navigation.navigate("new budget")}
             text="Create a Budget"
